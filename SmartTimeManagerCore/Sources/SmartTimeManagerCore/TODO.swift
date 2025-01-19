5a8cb9eede3c9b54ed6279b532de12f1084d3db6
Aleksei Krupinevich <aleksei.krupinevich@gmail.com>
2025-01-19 20:09:01 +0300

    Update TODO

--- SmartTimeManagerCore/Sources/SmartTimeManagerCore/TODO.swift
+++ SmartTimeManagerCore/Sources/SmartTimeManagerCore/TODO.swift
@@ -10,12 +10,17 @@
  ver. 1.5
  – Implement reorder feature
  – Refactor EditTaskView
- – Use Swift Data instead of Core Data (or refactor RealTasksRepository)
  – Implement notifications
  
  ver. 1.6
  – Redesign
  – Localize on different languages
  – Implement AI
+ 
+ Improve data storage
+ – Clear cache option in Settings
+ – UI for CoreData downloading
+ – Storage maintenance option in Settings
+ – Use Swift Data instead of Core Data (or refactor RealTasksRepository)
  
  */
