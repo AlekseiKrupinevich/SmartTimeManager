39bb40432a1569e7a929d62ec13d7c1cc73a97f8
Aleksei Krupinevich <aleksei.krupinevich@gmail.com>
2025-03-20 23:03:55 +0300

    Implement RealLogsInteractor

--- SmartTimeManagerCore/Sources/SmartTimeManagerCore/Interactors/Logs/RealLogsInteractor.swift
+++ SmartTimeManagerCore/Sources/SmartTimeManagerCore/Interactors/Logs/RealLogsInteractor.swift
@@ -42,31 +42,121 @@ class RealLogsInteractor: LogsInteractor {
         notesRepository.tags().count
     }
     
-    private var size_of_database: Int {
+    private var sizeOfDatabase: Int {
         CoreDataWrapper.sizeOfDatabase()
     }
     
+    private var urlSession: URLSession {
+        URLSession.shared
+    }
+    
+    private var url: URL {
+        URL(string: "https://aleksei-krupinevich.com:12321/logs/v1/")!
+    }
+    
+    private var appStartedBody: Data? {
+        struct Body: Encodable {
+            let instance_id: String
+            let device_model: String?
+            let os_version: String?
+            let app_version: String?
+        }
+        
+        let body = Body(
+            instance_id: instanceId,
+            device_model: deviceModel,
+            os_version: osVersion,
+            app_version: appVersion
+        )
+        
+        return try? JSONEncoder().encode(body)
+    }
+    
+    private var appDidBecomeActiveBody: Data? {
+        struct Body: Encodable {
+            let instance_id: String
+            let number_of_tasks: Int?
+            let number_of_notes: Int?
+            let number_of_tags: Int?
+            let size_of_database: Int?
+        }
+        
+        let body = Body(
+            instance_id: instanceId,
+            number_of_tasks: numberOfTasks,
+            number_of_notes: numberOfNotes,
+            number_of_tags: numberOfTags,
+            size_of_database: sizeOfDatabase
+        )
+        
+        return (try? JSONEncoder().encode(body))
+    }
+    
+    private func taskEventBody(_ taskEvent: TaskEvent) -> Data? {
+        struct Body: Encodable {
+            let instance_id: String
+            let event: String
+        }
+        
+        let body = Body(
+            instance_id: instanceId,
+            event: taskEvent.rawValue
+        )
+        
+        return (try? JSONEncoder().encode(body))
+    }
+    
+    private func noteEventBody(_ noteEvent: NoteEvent) -> Data? {
+        struct Body: Encodable {
+            let instance_id: String
+            let event: String
+        }
+        
+        let body = Body(
+            instance_id: instanceId,
+            event: noteEvent.rawValue
+        )
+        
+        return (try? JSONEncoder().encode(body))
+    }
+    
     func logAppStarted() {
-        print("Log: App started")
-        print("    instance_id: \(instanceId)")
-        print("    device_model: \(deviceModel)")
-        print("    os_version: \(osVersion)")
-        print("    app_version: \(appVersion ?? "")")
+        var request = URLRequest(
+            url: url.appending(component: "appStarted")
+        )
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = appStartedBody
+        urlSession.dataTask(with: request).resume()
     }
     
     func logAppDidBecomeActive() {
-        print("Log: App did become active")
-        print("    number_of_tasks: \(numberOfTasks)")
-        print("    number_of_notes: \(numberOfNotes)")
-        print("    number_of_tags: \(numberOfTags)")
-        print("    size_of_database: \(size_of_database)")
+        var request = URLRequest(
+            url: url.appending(component: "appDidBecomeActive")
+        )
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = appDidBecomeActiveBody
+        urlSession.dataTask(with: request).resume()
     }
     
     func logTaskEvent(_ taskEvent: TaskEvent) {
-        print("Log: Task event occurred <\(taskEvent.rawValue)>")
+        var request = URLRequest(
+            url: url.appending(component: "taskEvent")
+        )
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = taskEventBody(taskEvent)
+        urlSession.dataTask(with: request).resume()
     }
     
     func logNoteEvent(_ noteEvent: NoteEvent) {
-        print("Log: Note event occurred <\(noteEvent.rawValue)>")
+        var request = URLRequest(
+            url: url.appending(component: "noteEvent")
+        )
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = noteEventBody(noteEvent)
+        urlSession.dataTask(with: request).resume()
     }
 }
